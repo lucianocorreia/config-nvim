4a01ab7847694f386db0b4f5371de1a47906a101
Luciano Correia <luciano.correia@gmail.com>
2025-04-10 16:16:48 +0400

    update lazygit config

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -1,9 +1,12 @@
 return {
   'kdheepak/lazygit.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
   config = function()
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set('n', '<leader>lg', '<Cmd>LazyGit<CR>', { desc = 'Lazy [G]it' })
+    -- keymap.set('n', '<leader>lg', '<Cmd>LazyGit<CR>', { desc = 'Lazy [G]it' })
   end,
 }
