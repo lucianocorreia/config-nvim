0043164ce35eac6ce1eab8718200e937292f5768
Luciano Correia <luciano.correia@gmail.com>
2025-07-19 07:16:09 +0400

    update colorscheme

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,107 +1,29 @@
--- Colorscheme configuration for Neovim using Catppuccin theme
 -- return {
---   'catppuccin/nvim',
---   name = 'catppuccin',
+--   'sainnhe/everforest',
+--   lazy = false,
+--   priority = 1000,
 --   config = function()
---     require('catppuccin').setup {
---       flavour = 'mocha', -- latte, frappe, macchiato, mocha
---       background = { -- :h background
---         light = 'latte',
---         dark = 'mocha',
---       },
---       transparent_background = false, -- disables setting the background color.
---       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
---       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
---       dim_inactive = {
---         enabled = false, -- dims the background color of inactive window
---         shade = 'dark',
---         percentage = 0.15, -- percentage of the shade to apply to the inactive window
---       },
---       no_italic = false, -- Force no italic
---       no_bold = true, -- Force no bold
---       no_underline = true, -- Force no underline
---       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
---         comments = { 'italic' }, -- Change the style of comments
---         conditionals = { 'italic' },
---         loops = {},
---         functions = {},
---         keywords = {},
---         strings = {},
---         variables = {},
---         numbers = {},
---         booleans = {},
---         properties = {},
---         types = {},
---         operators = {},
---         -- miscs = {}, -- Uncomment to turn off hard-coded styles
---       },
---       color_overrides = {},
---       custom_highlights = function(colors)
---         return {
---           DiagnosticVirtualTextWarn = { fg = colors.overlay1, bg = colors.none, italic = true },
---         }
---       end,
---       default_integrations = true,
---       integrations = {
---         blink_cmp = true,
---         gitsigns = true,
---         nvimtree = true,
---         treesitter = true,
---         notify = true,
---         noice = true,
---         harpoon = true,
---         mason = true,
---         mini = {
---           enabled = true,
---           indentscope_color = '',
---         },
---         dropbar = {
---           enabled = true,
---           color_mode = false, -- enable color for kind's texts, not just kind's icons
---         },
---         indent_blankline = {
---           enabled = true,
---           scope_color = '', -- catppuccin color (eg. `lavender`) Default: text
---           colored_indent_levels = false,
---         },
---         copilot_vim = true,
---         snacks = {
---           enabled = true,
---           indent_scope_color = '', -- catppuccin color (eg. `lavender`) Default: text
---         },
---         nvim_surround = true,
---         flash = true,
---         lsp_trouble = true,
---         native_lsp = {
---           enabled = true,
---           virtual_text = {
---             errors = { 'italic' },
---             hints = { 'italic' },
---             warnings = { 'italic' },
---             information = { 'italic' },
---             ok = { 'italic' },
---           },
---           underlines = {
---             errors = { 'underline' },
---             hints = { 'underline' },
---             warnings = { 'underline' },
---             information = { 'underline' },
---             ok = { 'underline' },
---           },
---           inlay_hints = {
---             background = true,
---           },
---         },
---         which_key = true,
---         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
---       },
---     }
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.everforest_enable_italic = true
+--     vim.g.everforest_background = 'hard' -- 'soft', 'medium', or 'hard'
+--     vim.g.everforest_ui_contrast = 'high' -- 'low' or 'highkj
+--     vim.g.everforest_diagnostic_text_highlight = true
+--     vim.g.everforest_diagnostic_line_highlight = true
+--     vim.g.everforest_diagnostic_virtual_text = 'coloured' -- 'grey' or 'coloured'
+--     vim.g.everforest_transparent_background = false
+--     vim.g.everforest_better_performance = true
+--     vim.g.everforest_dim_inactive_windows = false
+--     vim.g.everforest_sign_column_background = 'none' -- 'grey' or 'none'
+--     vim.g.everforest_spell_foreground = false
+--     vim.g.everforest_show_eob = false
+--     vim.g.everforest_float_style = 'bright' -- 'bright' or 'dim'
+--     vim.g.everforest_inlay_hints_background = 'dimmed' -- 'none' or 'dimmed'
+--     vim.g.everforest_current_word = 'bold'
 --
---     -- setup must be called before loading
---     vim.cmd.colorscheme 'catppuccin'
+--     vim.cmd.colorscheme 'everforest'
 --   end,
 -- }
-
 -- *****************************************************************************************
 -- Everforest
 return {
@@ -339,5 +261,109 @@ return {
 --     }
 --
 --     vim.cmd.colorscheme 'kanagawa'
+--   end,
+-- }
+
+-- Colorscheme configuration for Neovim using Catppuccin theme
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   config = function()
+--     require('catppuccin').setup {
+--       flavour = 'mocha', -- latte, frappe, macchiato, mocha
+--       background = { -- :h background
+--         light = 'latte',
+--         dark = 'mocha',
+--       },
+--       transparent_background = false, -- disables setting the background color.
+--       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+--       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+--       dim_inactive = {
+--         enabled = false, -- dims the background color of inactive window
+--         shade = 'dark',
+--         percentage = 0.15, -- percentage of the shade to apply to the inactive window
+--       },
+--       no_italic = false, -- Force no italic
+--       no_bold = true, -- Force no bold
+--       no_underline = true, -- Force no underline
+--       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+--         comments = { 'italic' }, -- Change the style of comments
+--         conditionals = { 'italic' },
+--         loops = {},
+--         functions = {},
+--         keywords = {},
+--         strings = {},
+--         variables = {},
+--         numbers = {},
+--         booleans = {},
+--         properties = {},
+--         types = {},
+--         operators = {},
+--         -- miscs = {}, -- Uncomment to turn off hard-coded styles
+--       },
+--       color_overrides = {},
+--       custom_highlights = function(colors)
+--         return {
+--           DiagnosticVirtualTextWarn = { fg = colors.overlay1, bg = colors.none, italic = true },
+--         }
+--       end,
+--       default_integrations = true,
+--       integrations = {
+--         blink_cmp = true,
+--         gitsigns = true,
+--         nvimtree = true,
+--         treesitter = true,
+--         notify = true,
+--         noice = true,
+--         harpoon = true,
+--         mason = true,
+--         mini = {
+--           enabled = true,
+--           indentscope_color = '',
+--         },
+--         dropbar = {
+--           enabled = true,
+--           color_mode = false, -- enable color for kind's texts, not just kind's icons
+--         },
+--         indent_blankline = {
+--           enabled = true,
+--           scope_color = '', -- catppuccin color (eg. `lavender`) Default: text
+--           colored_indent_levels = false,
+--         },
+--         copilot_vim = true,
+--         snacks = {
+--           enabled = true,
+--           indent_scope_color = '', -- catppuccin color (eg. `lavender`) Default: text
+--         },
+--         nvim_surround = true,
+--         flash = true,
+--         lsp_trouble = true,
+--         native_lsp = {
+--           enabled = true,
+--           virtual_text = {
+--             errors = { 'italic' },
+--             hints = { 'italic' },
+--             warnings = { 'italic' },
+--             information = { 'italic' },
+--             ok = { 'italic' },
+--           },
+--           underlines = {
+--             errors = { 'underline' },
+--             hints = { 'underline' },
+--             warnings = { 'underline' },
+--             information = { 'underline' },
+--             ok = { 'underline' },
+--           },
+--           inlay_hints = {
+--             background = true,
+--           },
+--         },
+--         which_key = true,
+--         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+--       },
+--     }
+--
+--     -- setup must be called before loading
+--     vim.cmd.colorscheme 'catppuccin'
 --   end,
 -- }
