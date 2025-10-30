a2399fe3b7fbed2ccf2d86b55839f9db4338af6a
Luciano Correia <luciano.correia@gmail.com>
2025-10-30 16:12:37 +0400

    update config

--- lua/custom/plugins/dropbar.lua
+++ lua/custom/plugins/dropbar.lua
@@ -6,6 +6,18 @@ return {
     build = 'make',
   },
   config = function()
+    require('dropbar').setup {
+      bar = {
+        sources = function(buf, _)
+          local sources = require 'dropbar.sources'
+          return {
+            sources.path,
+            sources.lsp,
+          }
+        end,
+      },
+    }
+
     local dropbar_api = require 'dropbar.api'
     vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
     -- vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
