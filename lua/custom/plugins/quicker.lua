27791858031c6e84818f9658ae0888a885b0bbc7
Luciano Correia <luciano.correia@gmail.com>
2025-11-17 12:00:24 +0400

    update config

---
+++ lua/custom/plugins/quicker.lua
@@ -0,0 +1,40 @@
+return {
+  'stevearc/quicker.nvim',
+  event = 'VeryLazy',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    keys = {
+      {
+        '>',
+        function()
+          require('quicker').expand { before = 2, after = 2, add_to_existing = true }
+        end,
+        desc = 'Expandir contexto do quickfix',
+      },
+      {
+        '<',
+        function()
+          require('quicker').collapse()
+        end,
+        desc = 'Colapsar contexto do quickfix',
+      },
+    },
+  },
+  config = function(_, opts)
+    require('quicker').setup(opts)
+
+    -- Keymaps para abrir/fechar quickfix e loclist
+    vim.keymap.set('n', '<leader>q', function()
+      require('quicker').toggle()
+    end, {
+      desc = 'Toggle quickfix',
+    })
+
+    vim.keymap.set('n', '<leader>l', function()
+      require('quicker').toggle { loclist = true }
+    end, {
+      desc = 'Toggle loclist',
+    })
+  end,
+}
